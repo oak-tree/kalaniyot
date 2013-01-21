0aeb055f9b0d826a8e8c6d9d0b286bfa98d9fcbf
oak <k.o.alon@gmail.com>
2013-01-21 12:57:53 +0200

    new! base bull view

---
+++ kalaniyot/src/main/webapp/resources/js/view/bullItem.js
@@ -0,0 +1,64 @@
+
+/**
+ * basic 'bull' item
+ *  
+ *  
+ */
+window.BaseBullView = Backbone.View.extend({
+
+	tagName : "li",
+	//className:	"col",
+	template:"BullItemBasicView",
+    
+	
+	event: {
+		"click .image":"onImageClick",
+		"click .name" :"onNameClick",
+		"click .txt" :"nTxtClick",		
+		
+	},
+
+	onImageClick:function() {
+		alert('this function should be overwritten');
+	},
+	
+	onNameClick:function(){
+		alert('this function should be overwritten');	
+	},
+	
+	onTxtClick:function(){
+		alert('this function should be overwritten');
+		
+	},
+	
+	initialize : function(options) {
+		
+		//allow changing template from outside 
+		if (this.options.template != null )
+			this.template = this.options.template;
+		
+		//bind some events
+		this.model.bind("change", this.render, this);
+		this.model.bind("destroy", this.close, this);
+	},
+
+	render : function() {
+
+
+
+		var that = this;
+		TemplateManager.get(this.options.template, function(template) {
+
+			$(that.el).html(template(that.model.toJSON()));
+
+		});
+		return this;
+	},
+	
+	close : function() {
+		$(this.el).unbind();
+		$(this.el).empty();
+	}
+    
+
+});
